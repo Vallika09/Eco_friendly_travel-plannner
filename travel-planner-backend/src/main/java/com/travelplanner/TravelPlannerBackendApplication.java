8d2e887d826e31140f309b8e84752091bd255945
Vallika09 <vallikadhawan@gmail.com>
2025-02-20 19:30:10 +0530

    db

--- travel-planner-backend/src/main/java/com/travelplanner/TravelPlannerBackendApplication.java
+++ travel-planner-backend/src/main/java/com/travelplanner/TravelPlannerBackendApplication.java
@@ -2,12 +2,13 @@ package com.travelplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.travelplanner"})
+@EntityScan("com.travelplanner.model")  // Explicitly scan for entity classes
 public class TravelPlannerBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelPlannerBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TravelPlannerBackendApplication.class, args);
+    }
 }
