5a7722af6dce25998fe5f1fb286a0c0cb4390668
MitaliDogra <mitali22csu296@ncuindia.edu>
2025-02-22 19:56:10 +0530

    Merge branch 'main' of https://github.com/Vallika09/Eco_friendly_travel-plannner into main

--- travel-planner-backend/src/main/java/com/travelplanner/repository/RouteRepository.java
+++ travel-planner-backend/src/main/java/com/travelplanner/repository/RouteRepository.java
@@ -1,12 +1,14 @@
 package com.travelplanner.repository;
 
 import com.travelplanner.model.Route;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
 
-    List<Route> findBySourceAndDestination(String source, String destination);
+	List<Route> findBySourceAndDestination(String source, String destination);
 }
