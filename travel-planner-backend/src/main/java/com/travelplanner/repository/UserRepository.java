6719e8d02731f1cfac559a141a800b9ff0483927
hima <hima@LAPTOP-J9MC8ER3>
2025-04-05 11:29:12 +0530

    commit

---
+++ travel-planner-backend/src/main/java/com/travelplanner/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.travelplanner.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.travelplanner.model.User; 
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
